2938081fade544ab843c6a6d5906f7be39f5b36d
piteli <rushwender@gmail.com>
2020-05-27 23:49:24 +0800

    updated 1

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,15 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(home: Center(child: Text('Hello World'))));
+  runApp(MaterialApp(home: Scaffold(
+    appBar: AppBar(
+      title : Text("I Am Rich"),
+      backgroundColor: Colors.blueGrey[900],
+    ),
+      body : Center(
+          child : Image(
+              image : NetworkImage('https://4.img-dpreview.com/files/p/E~TS590x0~articles/3925134721/0266554465.jpeg')
+          )
+      )
+  )));
 }
